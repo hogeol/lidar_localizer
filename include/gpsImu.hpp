c1dc41091f23414aaf6a69413ce391eb74fa631a
hogeol <korkhg15@g.cbnu.ac.kr>
2022-07-25 10:32:37 +0900

    include/

---
+++ include/gpsImu.hpp
@@ -0,0 +1,15 @@
+#ifndef _GPS_IMU_
+#define _GPS_IMU_
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+namespace GpsImu{
+  class gpsImu{
+  
+  
+  };
+}
+
+#endif _GPS_IMU_
